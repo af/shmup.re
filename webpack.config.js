a409e50cb96e6541421b31270c5d6656a62d54d5
Aaron Franks <aaron.franks@gmail.com>
2017-05-19 13:35:27 -0700

    Use ES6 modules and rollup for a 3-4x reduction in pre-{min/gzip} filesize

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,25 @@ const path = require('path')
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 const {ANALYZE_BUNDLE} = process.env
 
+
 module.exports = {
   entry: {
-    main: './lib/js/src/main.js',
+    main: './lib/es6/src/main.js',
   },
   output: {
     path: path.join(__dirname, 'lib/bundle'),
     filename: '[name].js',
   },
-  plugins: ANALYZE_BUNDLE ? [new BundleAnalyzerPlugin()] : []
+
+  // Note: rollup makes this less informative; remove the rollup loader below to get useful results:
+  plugins: ANALYZE_BUNDLE ? [new BundleAnalyzerPlugin()] : [],
+
+  module: {
+    rules: [
+      {
+        test: /main.js$/,
+        use: [{ loader: 'webpack-rollup-loader', options: {}, }]
+      }
+    ]
+  }
 }
